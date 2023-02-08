fc059d0d5a55158db3c4dd7160ead6baebdae82b
Brendon Crowe <brendon.m.crowe@gmail.com>
2023-02-08 15:36:35 -0500

    Initial Commit

---
+++ HotelManzana/AddRegistrationTableViewController.swift
@@ -0,0 +1,117 @@
+//
+//  AddRegistrationTableViewController.swift
+//  HotelManzana
+//
+//  Created by Brendon Crowe on 2/8/23.
+//
+
+import UIKit
+
+class AddRegistrationTableViewController:
+    UITableViewController {
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var checkInDateLabel: UILabel!
+    @IBOutlet weak var checkInDatePicker: UIDatePicker!
+    @IBOutlet weak var checkOutDateLabel: UILabel!
+    @IBOutlet weak var checkOutDatePicker: UIDatePicker!
+    
+    let checkInDateLabelCellIndexPath = IndexPath(row: 0, section: 1) // sets the index path
+    let checkInDatePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    
+    let checkOutDateLabelCellIndexPath = IndexPath(row: 2, section: 1)
+    let checkOutDatePickerCellIndexPath = IndexPath(row: 3, section: 1)
+    
+    var isCheckInDatePickerVisible: Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerVisible
+        }
+    }
+
+    var isCheckOutDatePickerVisible: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerVisible
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateDateViews()
+        let midnightToday = Calendar.current.startOfDay(for: Date()) // configuring the minimum date
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
+        
+    }
+    
+    private func updateDateViews() { // formatting the date to a String
+        checkOutDatePicker.minimumDate = Calendar.current.date(byAdding: .day, value: 1, to: checkInDatePicker.date)
+        checkInDateLabel.text = checkInDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+        checkOutDateLabel.text = checkOutDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+    }
+    
+    
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        updateDateViews()
+    }
+    
+    @IBAction func doneBarButtonPressed(_ sender: UIBarButtonItem) {
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let email = emailTextField.text ?? ""
+        let checkInDate = checkOutDatePicker.date
+        let checkOutDate = checkOutDatePicker.date
+        
+        print("Button Tapped")
+        print("first name: \(firstName)")
+        print("last name: \(lastName)")
+        print("email: \(email)")
+        print("check in: \(checkInDate)")
+        print("check out: \(checkOutDate)")
+    }
+    
+    // This function collapses the Date Pickers, leaving more space for the table view.
+    // where acts as an if statement. "This case where ... is true"
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerCellIndexPath where isCheckInDatePickerVisible == false:
+            return 0 // return 0 for the height of the checkInDatePicker
+        case checkOutDatePickerCellIndexPath where isCheckOutDatePickerVisible == false:
+            return 0 // // return 0 for the height of the checkOutDatePicker
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    // This function sets the row height for the Date Picker
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerCellIndexPath:
+            return 190
+        case checkOutDatePickerCellIndexPath:
+            return 190
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath == checkInDateLabelCellIndexPath && isCheckOutDatePickerVisible == false {
+            // check-in label selected, check-out picker is not visible, toggle check-in picker
+            isCheckInDatePickerVisible.toggle()
+        } else if indexPath == checkOutDateLabelCellIndexPath && isCheckInDatePickerVisible == false {
+            // check-out label selected, check-in picker is not visible, toggle check-out picker
+            isCheckOutDatePickerVisible.toggle()
+        } else if indexPath == checkInDateLabelCellIndexPath || indexPath == checkOutDateLabelCellIndexPath {
+            // either label was selected, previous conditions failed meaning at least one picker is visible, toggle both
+            isCheckInDatePickerVisible.toggle()
+            isCheckOutDatePickerVisible.toggle()
+        } else {
+            return
+        }
+        tableView.beginUpdates() // instruct the table view to update itself so that the height for each row is recalculated
+        tableView.endUpdates()
+    }
+}
